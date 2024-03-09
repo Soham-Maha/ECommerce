a7b8267eb41634eb1e92d4a5d89bf858c22ea2eb
Soham-Maha <soham.mahapatra2023@gmail.com>
2024-03-09 18:19:51 +0530

    -

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 const PORT = 5000;
 const dbconnection = require("./config/DBconnection.js");
-const cookieparser = require('cookie-parser');
-const cors = require('cors');
-const userRoutes = require('./route/users/usersRoutes.js')
-const productRoutes = require("./route/products/productRoutes.js")
-
+const cookieparser = require("cookie-parser");
+const cors = require("cors");
+const userRoutes = require("./route/users/usersRoutes.js");
+const productRoutes = require("./route/products/productRoutes.js");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 
-app.use(cors({origin: true, credentials: true}))
+app.use(cors({ origin: true, credentials: true }));
 
-app.get('/',(req,res)=>{
-    res.send("Hello World!")
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/api/users',userRoutes)
-app.use('/api/products',productRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 //connect to the database mongodb
 dbconnection();
 
-app.listen(process.env.PORT || 5000,()=>{
-    console.log(`Server is running on port : ${PORT}`)
-})
-
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port : ${PORT}`);
+});
